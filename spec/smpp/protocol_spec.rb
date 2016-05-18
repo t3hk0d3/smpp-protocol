68f82ce262e65439feba9b12706f2902a3e881e3
Igor Yamolov <iyamolov@spbtv.com>
2016-05-19 01:35:33 +0300

    Remove stub spec

--- spec/smpp/protocol_spec.rb
+++ spec/smpp/protocol_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
 describe Smpp::Protocol do
-  it 'has a version number' do
-    expect(Smpp::Protocol::VERSION).not_to be nil
-  end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
